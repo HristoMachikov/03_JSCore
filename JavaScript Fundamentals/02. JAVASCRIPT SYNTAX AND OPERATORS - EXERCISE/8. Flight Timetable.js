2f7ffa81d09f0ce3505bc038ee85c45d97f8730e
HristoMachikov <hristo2635@abv.bg>
2019-01-17 17:47:19 +0200

    update files

--- JavaScript Fundamentals/02. JAVASCRIPT SYNTAX AND OPERATORS - EXERCISE/8. Flight Timetable.js
+++ JavaScript Fundamentals/02. JAVASCRIPT SYNTAX AND OPERATORS - EXERCISE/8. Flight Timetable.js
@@ -1,5 +1,5 @@
 function solve(inputArray) {
-    console.log(`${inputArray[0]}: Destination – ${inputArray[1]}, Flight – ${inputArray[3]}, Time – ${inputArray[2]}, Gate - ${inputArray[4]}`);
+    console.log(`${inputArray[0]}: Destination - ${inputArray[1]}, Flight - ${inputArray[3]}, Time - ${inputArray[2]}, Gate - ${inputArray[4]}`);
 
 };
 solve(['Arrivals', 'Paris', '02:22', 'VD17', '3']);
--- JavaScript Fundamentals/02. JAVASCRIPT SYNTAX AND OPERATORS - EXERCISE/9. Calorie Object.js
+++ JavaScript Fundamentals/02. JAVASCRIPT SYNTAX AND OPERATORS - EXERCISE/9. Calorie Object.js
@@ -1,12 +1,10 @@
-function solve(input){
+function solve(inputArr) {
     let output = {};
-    let arr
-    input.forEach(function(element) {
-        
-    }
-    for (let i = 0; i < input){
-
-    }
+    for (let i = 0; i < inputArr.length; i += 2) {
+        let objKey = inputArr[i];
+        let objValue = +inputArr[i + 1];
+        output[objKey] = objValue;
+    };
     console.log(output);
 };
-solve(['Yoghurt', 48, 'Rise', 138, 'Apple', 52]);
+solve(['Potato', 93, 'Skyr', 63, 'Cucumber', 18, 'Milk', 42]);
