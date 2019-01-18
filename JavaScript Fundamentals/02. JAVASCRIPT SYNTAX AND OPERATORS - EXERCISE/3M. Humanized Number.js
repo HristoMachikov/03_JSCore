2ed9da821246d48ac58d8544d4078580c73a340f
HristoMachikov <hristo2635@abv.bg>
2019-01-18 17:04:33 +0200

    add files

---
+++ JavaScript Fundamentals/02. JAVASCRIPT SYNTAX AND OPERATORS - EXERCISE/3M. Humanized Number.js
@@ -0,0 +1,5 @@
+function solve(inputString) {
+    let inputArr = inputString.split(" ");
+    console.log(inputArr);
+};
+solve('The school has 256 students. In each class there are 26 chairs, 13 desks and 1 board.');
