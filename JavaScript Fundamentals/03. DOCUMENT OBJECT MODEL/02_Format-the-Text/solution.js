911f7eb2dc6643582e6b6196c4158bb8f0bcbfba
HristoMachikov <hristo2635@abv.bg>
2019-01-19 21:18:21 +0200

    add file

--- JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/02_Format-the-Text/solution.js
+++ JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/02_Format-the-Text/solution.js
@@ -1,33 +1,33 @@
 function solve() {
   let inputTextElem = document.getElementById('input');
-  let inputTextElemContent = inputTextElem.innerHTML;
+  let inputTextElemContent = inputTextElem.textContent;
 
-  let outputArray = inputTextElemContent.split('. ');
-
+  let outputArray = inputTextElemContent.replace(/\s+/g, " ").split(". ");
   let outputThreeSentArr = [];
   for (let i = 0; i < outputArray.length; i += 3) {
-    let currSent = outputArray[i];
+    let currSent = "";
     let currArrLen = outputArray.length - i;
-    
     let count = 0;
     if (Math.floor(currArrLen / 3) > 0) {
       count = 3;
     } else {
       count = currArrLen % 3;
     };
-    for (let j = 0; j < count - 1; j++) {
-      currSent = outputArray[i + j + 1] + ". ";
-    }
-    console.log(currSent);
+    for (let j = 0; j < count; j++) {
+     // if (outputArray[i + j].replace(" ", "") != "") {
+        if (i + j == outputArray.length - 1) {
+          currSent += [outputArray[i + j]];
+        } else {
+          currSent += outputArray[i + j] + ". ";
+        };
+      //};
+    };
     outputThreeSentArr.push(currSent);
-    
   }
   let outputTextElem = document.getElementById('output');
-
-  let ouputNewParElem = document.createElement('p');
-  //let outputResult = 
   outputThreeSentArr.forEach(element => {
+    let ouputNewParElem = document.createElement("p");
     ouputNewParElem.textContent = element;
-    outputTextElem.appendChild(ouputNewParElem)
+    outputTextElem.appendChild(ouputNewParElem);
   });
 }
