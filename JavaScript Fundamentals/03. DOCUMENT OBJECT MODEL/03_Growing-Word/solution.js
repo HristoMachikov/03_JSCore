6ceeedc2bfb6a731371329ddae5f9bdd505d45ec
HristoMachikov <hristo2635@abv.bg>
2019-01-21 00:12:15 +0200

    add files

--- JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/03_Growing-Word/solution.js
+++ JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/03_Growing-Word/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-   //TODO 
-}
+    let numbOfClicks = 0;
+    let lisenerOfClicks = document.querySelector('button').addEventListener('click', addClick, false);
+    function addClick() {
+        numbOfClicks += 1;
+        let currentColoor = "";
+        let numbOfSize = numbOfClicks * 2;
+        switch (numbOfClicks % 3) {
+            case 1: currentColoor = "blue"; break;
+            case 2: currentColoor = "green"; break;
+            case 0: currentColoor = "red"; break;
+        };
+        let newPragElem = document.createElement("p");
+        newPragElem.setAttribute("style", `color: ${currentColoor}; font-size: ${numbOfSize}px;`);
+        newPragElem.innerHTML = "Growing Word";
+        let parentElem = document.getElementById("exercise");
+        let oldPargElem = parentElem.getElementsByTagName('p');
+        parentElem.replaceChild(newPragElem, oldPargElem[0]);
+    };
+};
--- JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/04_Register-Notification/solution.js
+++ JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/04_Register-Notification/solution.js
@@ -1,4 +1,39 @@
+function register() {
+  let usernameElem = document.getElementById("username");
+  let emailElem = document.getElementById("email");
+  let passwordElem = document.getElementById("password");
 
- function register() {
-   //TODO
- }
+  let currentUsername = usernameElem.value;
+  let currentEmail = emailElem.value;
+  let currentPassword = passwordElem.value;
+
+  let resultSection = document.getElementById("result");
+
+  if (currentUsername != "" && currentEmail.match(/(.+)@(.+).(com|bg)/gm) && currentPassword != "") {
+    let currentResultElem1 = document.createElement('h1');
+    currentResultElem1.textContent = "Successful Registration!";
+    //let currentResultElem2 = document.createTextNode(`Username: ${currentUsername}` + "\r\n" + `Email: ${currentEmail}` + "\r\n" + `Password: ${"*".repeat(currentPassword.length)}`);
+    let currentResultElem2 = document.createTextNode(`Username: ${currentUsername}`);
+    let currentResultElem3 = document.createTextNode(`Email: ${currentEmail}`);
+    let currentResultElem4 = document.createTextNode(`Password: ${"*".repeat(currentPassword.length)}`);
+    let newline = document.createElement('br');
+    let newline2 = document.createElement('br');
+    //let currentResultElem = currentResultElem1 + currentResultElem2;
+    resultSection.appendChild(currentResultElem1);
+    resultSection.appendChild(currentResultElem2);
+    resultSection.appendChild(newline);
+    resultSection.appendChild(currentResultElem3);
+    resultSection.appendChild(newline2);
+    resultSection.appendChild(currentResultElem4);
+    //resultSection.innerHTML = `Username: ${currentUsername}<br>Email: ${currentEmail}<br>Password: ${"*".repeat(currentPassword.length)}`;
+    setTimeout(function () {
+      resultSection.textContent = "";
+    }, 5000);
+    //  setTimeout(function () { 
+    //   location.reload();
+    // }, 5000);
+  };
+  usernameElem.value = "";
+  emailElem.value = "";
+  passwordElem.value = "";
+};
