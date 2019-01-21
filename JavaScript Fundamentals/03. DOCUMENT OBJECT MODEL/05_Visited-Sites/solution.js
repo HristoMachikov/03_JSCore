4ea5ba180267085da62b1b43ae1bdb0b095a3eb0
HristoMachikov <hristo2635@abv.bg>
2019-01-21 12:16:04 +0200

    add files

--- JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/05_Visited-Sites/solution.js
+++ JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/05_Visited-Sites/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
-}
+  let currentSite = document.getElementsByTagName('a');
+  console.log(currentSite);
+  let currentSiteContent = "";
+  let lisenerOfClicks = document.querySelector('a').addEventListener('click', addClick, false);
+  function addClick() {
+    console.log(lisenerOfClicks);
+    for (element of currentSite) {
+      if (element.textContent != "" && element.getAttribute('href') == "#") {
+        let numbOfClicks = 0;
+
+        numbOfClicks += 1;
+        console.log(numbOfClicks);
+        currentSiteContent = element.textContent;
+      };
+    };
+  };
+};
