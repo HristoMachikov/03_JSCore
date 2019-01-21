ffe964b6da78b4fe83fc3f3f1439ada1ccbc3816
HristoMachikov <hristo2635@abv.bg>
2019-01-21 23:44:24 +0200

    add files

--- JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/04_Register-Notification/solution.js
+++ JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/04_Register-Notification/solution.js
@@ -12,26 +12,21 @@ function register() {
   if (currentUsername != "" && currentEmail.match(/(.+)@(.+).(com|bg)/gm) && currentPassword != "") {
     let currentResultElem1 = document.createElement('h1');
     currentResultElem1.textContent = "Successful Registration!";
-    //let currentResultElem2 = document.createTextNode(`Username: ${currentUsername}` + "\r\n" + `Email: ${currentEmail}` + "\r\n" + `Password: ${"*".repeat(currentPassword.length)}`);
+  
     let currentResultElem2 = document.createTextNode(`Username: ${currentUsername}`);
     let currentResultElem3 = document.createTextNode(`Email: ${currentEmail}`);
     let currentResultElem4 = document.createTextNode(`Password: ${"*".repeat(currentPassword.length)}`);
-    let newline = document.createElement('br');
-    let newline2 = document.createElement('br');
-    //let currentResultElem = currentResultElem1 + currentResultElem2;
+
     resultSection.appendChild(currentResultElem1);
     resultSection.appendChild(currentResultElem2);
-    resultSection.appendChild(newline);
+    resultSection.appendChild(document.createElement('br'));
     resultSection.appendChild(currentResultElem3);
-    resultSection.appendChild(newline2);
+    resultSection.appendChild(document.createElement('br'));
     resultSection.appendChild(currentResultElem4);
-    //resultSection.innerHTML = `Username: ${currentUsername}<br>Email: ${currentEmail}<br>Password: ${"*".repeat(currentPassword.length)}`;
+
     setTimeout(function () {
       resultSection.textContent = "";
     }, 5000);
-    //  setTimeout(function () { 
-    //   location.reload();
-    // }, 5000);
   };
   usernameElem.value = "";
   emailElem.value = "";
--- JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/05_Visited-Sites/solution.js
+++ JavaScript Fundamentals/03. DOCUMENT OBJECT MODEL/05_Visited-Sites/solution.js
@@ -1,28 +1,14 @@
 function solve() {
-  //let currentSite = document.getElementsByTagName('a');
-  //console.log(currentSite);
   let currentSiteContent = "";
-  let lisenerOfClicks = document.addEventListener('click', function (e) {
-    let currentTarget = e.target;
-    currentSiteContent = currentTarget.textContent;
-
-
-    let check = false;
-    switch (currentSiteContent) {
-      case "SoftUni":
-      case "Google":
-      case "YouTube":
-      case "Wikipedia":
-      case "Gmail":
-      case "Stackoverflow": check = true; break;
-      default: break;
-    };
-    let currentCount = 0;
-    if (check) {
+  Array.from(document.getElementsByTagName('a')).forEach(elem => {
+    elem.addEventListener('click', listenedResult);
+    console.log(elem);
+    function listenedResult(event) {
+      let currentTarget = event.target;
+      currentSiteContent = currentTarget.textContent;
       let nextElem = currentTarget.nextElementSibling;
-       currentCount = Number(nextElem.textContent.split(" ")[1]);
+      currentCount = Number(nextElem.textContent.split(" ")[1]);
       nextElem.textContent = `Visited: ${currentCount + 1} times`;
     };
-    return currentCount + 1;
-  }, false);
+  });
 };
