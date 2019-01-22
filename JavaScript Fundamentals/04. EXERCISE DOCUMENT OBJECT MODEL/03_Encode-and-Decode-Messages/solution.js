775d7160cdf2e684e82b3ba89d8070f248cd5996
HristoMachikov <hristo2635@abv.bg>
2019-01-22 21:57:45 +0200

    add files

--- JavaScript Fundamentals/04. EXERCISE DOCUMENT OBJECT MODEL/03_Encode-and-Decode-Messages/solution.js
+++ JavaScript Fundamentals/04. EXERCISE DOCUMENT OBJECT MODEL/03_Encode-and-Decode-Messages/solution.js
@@ -1,3 +1,12 @@
-function solve() {
-	//TODO
+function solve(a) {
+	// Array.from(document.getElementsByTagName("button")).forEach(function(btn) {
+	// 	btn.addEventListener('click', doSometing);
+	// 	function doSometing(elem){
+
+			
+	// 	}
+	// });
+
+	let charArr = a.split('');
 }
+solve("This program should contain");
