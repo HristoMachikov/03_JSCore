dbd653bec0004aed5b16343f975bbe0edbe1f1f6
HristoMachikov <hristo2635@abv.bg>
2019-01-24 00:04:05 +0200

    add files

--- JavaScript Fundamentals/04. EXERCISE DOCUMENT OBJECT MODEL/03_Encode-and-Decode-Messages/solution.js
+++ JavaScript Fundamentals/04. EXERCISE DOCUMENT OBJECT MODEL/03_Encode-and-Decode-Messages/solution.js
@@ -1,12 +1,28 @@
-function solve(a) {
-	// Array.from(document.getElementsByTagName("button")).forEach(function(btn) {
-	// 	btn.addEventListener('click', doSometing);
-	// 	function doSometing(elem){
+function solve() {
+	let btnArray = Array.from(document.getElementsByTagName('button'));
+	btnArray.forEach(function (btn) {
+		btn.addEventListener('click', doSometing);
+		function doSometing(elem) {
+			let currentBtnElem = elem.target
+			let currentParentElem = currentBtnElem.parentNode;
+			let currMassageElem = currentParentElem.children[1];
+			let decodeStr = "";
+			let encodeStr = "";
+			if (currentBtnElem.textContent == "Encode and send it") {
+				currMassageElem.value.split('').forEach(element => {
+					let deCurrChar = element.charCodeAt(0) + 1;
+					decodeStr += String.fromCharCode(deCurrChar);
+				});
 
-			
-	// 	}
-	// });
-
-	let charArr = a.split('');
-}
-solve("This program should contain");
+				btnArray[1].parentNode.children[1].value = decodeStr;
+				currMassageElem.value = "";
+			} else if (currentBtnElem.textContent == "Decode and read it") {
+				currMassageElem.value.split('').forEach(elem => {
+					let enCurrChar = elem.charCodeAt(0) - 1;
+					encodeStr += String.fromCharCode(enCurrChar);
+				});
+				btnArray[1].parentNode.children[1].value = encodeStr;
+			}
+		}
+	});
+};
--- JavaScript Fundamentals/04. EXERCISE DOCUMENT OBJECT MODEL/05_Number-Convertor/solution.js
+++ JavaScript Fundamentals/04. EXERCISE DOCUMENT OBJECT MODEL/05_Number-Convertor/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-//TODO
-}
+    let hexaElem = document.createElement('option');
+    hexaElem.textContent = "Hexadecimal";
+    let binaryElem = document.createElement('option');
+    binaryElem.textContent = "Binary"
+    document.getElementById('selectMenuTo').appendChild(hexaElem);
+    document.getElementById('selectMenuTo').appendChild(binaryElem);
+    let convertBtn = Array.from(document.getElementsByTagName('button'))[0];
+    convertBtn.addEventListener('click', calculate);
+    function calculate(event) {
+        let currentNumb = document.getElementById('input').value;
+        let convertNumb = "";
+        let currSelectElem = document.getElementById("selectMenuTo");
+        let currDim = currSelectElem.options[currSelectElem.selectedIndex].value;
+        if (currDim == "Hexadecimal" && currentNumb != "") {
+            convertNumb = parseInt(currentNumb, 10).toString(16);
+        } else if (currDim == "Binary" && currentNumb != "") {
+            convertNumb = parseInt(currentNumb, 10).toString(2);
+        };
+        let convertElem = document.getElementById('result');
+        convertElem.value = convertNumb.toUpperCase();
+    };
+};
