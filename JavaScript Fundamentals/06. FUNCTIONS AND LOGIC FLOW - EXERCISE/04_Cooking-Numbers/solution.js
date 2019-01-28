3bc04961a95c99b5da36a96d19d3dd344be0bc3d
HristoMachikov <hristo2635@abv.bg>
2019-01-28 12:31:24 +0200

    add files

--- JavaScript Fundamentals/06. FUNCTIONS AND LOGIC FLOW - EXERCISE/04_Cooking-Numbers/solution.js
+++ JavaScript Fundamentals/06. FUNCTIONS AND LOGIC FLOW - EXERCISE/04_Cooking-Numbers/solution.js
@@ -1,3 +1,43 @@
 function solve() {
-    //TODO...
+    let inputNumbElem = document.querySelector('#exercise input[type="number"]');
+    let inputNumb = "";
+    let btnArr = Array.from(document.querySelectorAll('#exercise button'));
+    let resultElem = document.getElementById("output");
+    let result = "";
+    let counter = 0;
+
+    btnArr.forEach(element => {
+        element.addEventListener('click', cookingNumber);
+    });
+
+    function cookingNumber(event) {
+        if (inputNumbElem.value === "") {
+            inputNumbElem.value = 0
+        };
+        if (counter === 0) {
+            inputNumb = Number(inputNumbElem.value);
+        }
+        let resultNumb = inputNumb;
+        let currAction = event.target.textContent;
+        let outputNumb = calc(resultNumb, currAction)
+        inputNumb = +outputNumb;
+        resultElem.textContent = outputNumb;
+        counter++;
+    };
+
+    function calc(numb, action) {
+        let result = "";
+        if (action === "Chop") {
+            result = numb * 0.5;
+        } else if (action === "Dice") {
+            result = Math.sqrt(numb);
+        } else if (action === "Spice") {
+            result = numb + 1;
+        } else if (action === "Bake") {
+            result = numb * 3;
+        } else if (action === "Fillet") {
+            result = 0.8 * numb;
+        }
+        return result;
+    };
 }
--- JavaScript Fundamentals/06. FUNCTIONS AND LOGIC FLOW - EXERCISE/05_Cards-Generator/solution.js
+++ JavaScript Fundamentals/06. FUNCTIONS AND LOGIC FLOW - EXERCISE/05_Cards-Generator/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-    //TODO...
+    let arrCart = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+    let resultSecElem = document.getElementById("cards");
+
+    let btnElem = document.querySelector('#exercise button');
+    btnElem.addEventListener('click', cardsGenerator);
+    function cardsGenerator(event) {
+        let fromCardElem = document.getElementById("from");
+        let toCardElem = document.getElementById("to");
+        let selectElem = document.querySelector('#exercise select');
+        let currSuit = selectElem.options[selectElem.selectedIndex].value;
+        let currSuitChar = currSuit[0];
+
+        console.log(currSuitChar);
+        console.log(toCardElem.value);
+        console.log(fromCardElem.value)
+    }
+
 }
