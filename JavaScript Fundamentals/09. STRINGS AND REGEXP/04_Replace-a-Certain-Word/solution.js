08dc549d08056fa55420b003c99fcad6f07626bb
HristoMachikov <hristo2635@abv.bg>
2019-01-31 12:18:29 +0200

    add files

--- JavaScript Fundamentals/09. STRINGS AND REGEXP/04_Replace-a-Certain-Word/solution.js
+++ JavaScript Fundamentals/09. STRINGS AND REGEXP/04_Replace-a-Certain-Word/solution.js
@@ -4,9 +4,29 @@ function solve() {
   let resultElem = document.getElementById('result');
 
   let matchWord = (inputArr[0].split(' ').filter(x => x))[2].toLowerCase();
-  let resultArr = inputArr.forEach(x => {
-    x.map( e => {e.replace(matchWord.toLowerCase(), inputStr)})
+  //let resultArr = 
+  inputArr.forEach((x, index, arr) => {
+    console.log(x)
+    let match = /matchWord/gi;
+    let search = x.replace(match, inputStr)
+    arr[index] = search;
+    console.log(search)
+    // x.split(' ')
+    //   //.filter(w => w !== "")
+    //   .map((word, index, arr) => {
+    //     let match = matchWord.exec(word.toLowerCase())
+    //     if (match) {
+    //       word.replace(match, inputStr)
+    //     }
+    //     // if (word.toLowerCase() == matchWord) {
+    //     //   arr[index] = inputStr;
+    //     //   console.log(arr[index])
+    //     // }
+    //     // console.log(word)
+    //   }).join(' ')
+    // // w.toLowerCase() == matchWord)?
+    // //map( e => {e.replace(matchWord.toLowerCase(), inputStr)})
   });
   console.log(inputArr)
-  console.log(resultArr)
+  //console.log(resultArr)
 }
--- JavaScript Fundamentals/09. STRINGS AND REGEXP/05_Extract-User-Data/solution.js
+++ JavaScript Fundamentals/09. STRINGS AND REGEXP/05_Extract-User-Data/solution.js
@@ -3,26 +3,21 @@ function solve() {
   let resultElem = document.getElementById('result');
   let pElem = document.createElement('p');
   let invalidDataPara = pElem.cloneNode();
-  invalidDataPara.textContent = 'Invalid Data';
-  let dashElem = document.pElem.cloneNode();
-  dashElem.textContent = '---';
+  invalidDataPara.textContent = 'Invalid data';
+  let dashElem = pElem.cloneNode();
+  dashElem.textContent = '- - -';
 
   let pattern = /^([A-Z][a-z]* [A-Z][a-z]*) (\+359 \d \d{3} \d{3}|\+359-\d-\d{3}-\d{3}) ([a-z0-9]+@[a-z]+\.[a-z]{2,3})$/g
   inputArr.forEach(element => {
     let match = pattern.exec(element)
     if (match) {
-      let validElem = pElem.cloneNode();
-      validElem.textContent = `Name: ${match[1]}`;
-      resultElem.appendChild(validElem);
-
       insertValidElem(`Name: ${match[1]}`);
       insertValidElem(`Phone Number: ${match[2]}`);
       insertValidElem(`Email: ${match[3]}`);
-
     } else {
       resultElem.appendChild(invalidDataPara.cloneNode(true));
     }
-    //.forEach(match)
+
     resultElem.appendChild(dashElem.cloneNode(true));
   })
   function insertValidElem(text) {
@@ -31,4 +26,3 @@ function solve() {
     resultElem.appendChild(validElem);
   }
 }
-
