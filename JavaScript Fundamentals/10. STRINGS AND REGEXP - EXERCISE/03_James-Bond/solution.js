7a2e991a0cc5d46efe6de5af944c131c5ad6bb65
HristoMachikov <hristo2635@abv.bg>
2019-02-02 16:28:35 +0200

    add files

--- JavaScript Fundamentals/10. STRINGS AND REGEXP - EXERCISE/03_James-Bond/solution.js
+++ JavaScript Fundamentals/10. STRINGS AND REGEXP - EXERCISE/03_James-Bond/solution.js
@@ -1,3 +1,51 @@
 function solve() {
-  //TODO...
+  let inputArr = JSON.parse(document.getElementById('arr').value);
+  let resultElem = document.getElementById('result');
+
+  console.log(inputArr);
+
+  let key = inputArr[0];
+  let text = inputArr[1];
+
+  let specialKey = new RegExp(`(^| )${key}`, "gi");
+  let pattern = /^ +[A-Z!%$#]{8,}([ .,]|$)/g;
+  let matchArr;
+  //let cuttedText = text;
+  console.log(specialKey);
+  while ((matchArr = specialKey.exec(text)) !== null) {
+    //debugger
+    if (text[specialKey.lastIndex] === ' ') {
+      let cuttedText = text.slice(specialKey.lastIndex);
+      let currMatch = cuttedText.match(pattern);
+      if (currMatch) {
+        let replaceMess = currMatch[0].trim();
+        let encodMess = replaceMess.split('').map(a => a.toLowerCase())
+          // .map((ch, index, arr) => {
+          //   let currChar;
+          //   if (ch === '!') {
+          //     ch.replace(ch, "1")
+          //   } else {
+          //     ch.toLowerCase();
+          //   }
+          //   // switch (ch) {
+          //   //   case '!': currChar = 1;
+          //   //   case '%': currChar = 2;
+          //   //   case '#': currChar = 3;
+          //   //   case '$': currChar = 4;
+          //   //   default: currChar = ch.toLowerCase();
+          //   // }
+          //   //return currChar;
+          // }
+          // )
+          .join('');
+        text.replace(replaceMess, encodMess);
+        console.log(encodMess);
+      }
+    }
+
+  }
+
+  console.log(key);
+  console.log(text);
+
 }
--- JavaScript Fundamentals/10. STRINGS AND REGEXP - EXERCISE/04_Airport-Check/solution.js
+++ JavaScript Fundamentals/10. STRINGS AND REGEXP - EXERCISE/04_Airport-Check/solution.js
@@ -1,21 +1,34 @@
 function solve() {
     let stringElem = document.getElementById('str');
-    let result = document.getElementById('result');
-
+    let resultElem = document.getElementById('result');
     let [text, wantedInfo] = stringElem.value.split(', ');
 
-    let namePattern = / ([A-Z]+([a-zA-Z]*)?)(-[A-Z][a-zA-Z]*\.*)?-([A-Z][a-zA-Z]*)? /g;
+    let namePattern = / ([A-Z]+([A-Za-z]*)?)(-[A-Z][A-Za-z]*\.)?-([A-Z][A-Za-z]*)? /g;
+    //                / [A-Z]+[a-z]*(-[A-Z]+[a-z]*\.)?-[A-Z]+[a-z]* /g;
+    //let namePattern = / ([A-Z]+([a-zA-Z]*)?)(-[A-Z][a-zA-Z]*\.)?-([A-Z][a-zA-Z]*)? /g;
     let airportPattern = / [A-Z]{3}\/[A-Z]{3} /g;
     let fligthNumberPattern = / [A-Z]{1,3}[\d]{1,5} /g;
+    let companyPattern = /- ([A-Z]+([A-Za-z]*)?)\*([A-Z][A-Za-z]*)? /g;
+    //- [A-Z]+[a-z]*\*[A-Z]+[a-z]* /g;
 
     if (wantedInfo === "name") {
-        let matchInfo = text.match(namePattern)[0];
-        let name = matchInfo[0].trim().replace('-', " ");
-        result.textContent = `Mr/Ms, ${name}, have a nice flight!`;
-    }else if (wantedInfo === 'flight'){
+        let matchName = text.match(namePattern)[0];
+        let name = matchName.trim().replace('-', " ");
+        resultElem.textContent = `Mr/Ms, ${name}, have a nice flight!`;
+    } else if (wantedInfo === 'flight') {
         let airport = text.match(airportPattern)[0].trim().split("/");
         let flightNumber = text.match(fligthNumberPattern)[0].trim();
-        result.textContent = `Your flight number ${flightNumber} is from ${airport[0]} to ${airport[1]}.`;
+        resultElem.textContent = `Your flight number ${flightNumber} is from ${airport[0]} to ${airport[1]}.`;
+    } else if (wantedInfo === 'company') {
+        let companyName = text.match(companyPattern)[0].slice(1).trim(' ').split('*').join(' ');
+        resultElem.textContent = `Have a nice flight with ${companyName}.`;
+    } else if (wantedInfo === 'all') {
+        let matchName = text.match(namePattern)[0];
+        
+        let name = matchName.trim().replace('-', " ");
+        let airport = text.match(airportPattern)[0].trim().split("/");
+        let flightNumber = text.match(fligthNumberPattern)[0].trim();
+        let companyName = text.match(companyPattern)[0].slice(1).trim(' ').split('*').join(' ');
+        resultElem.textContent = `Mr/Ms, ${name}, your flight number ${flightNumber} is from ${airport[0]} to ${airport[1]}. Have a nice flight with ${companyName}.`;
     }
-    
 }
